175a3060f515eb4205eab06387b179ed3f149bcb
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-12-06 11:07:15 -0300

    reactboostrap add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import PrimarySearchAppBar from './PrimarySearchAppBar';
 import LoginStyled from './Components/Login/Login'
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
