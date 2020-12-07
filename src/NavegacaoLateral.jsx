669575c4ff2dd109eeee85e5a58f1a9bb13bc573
MarckRDA <marcos.edo132@gmail.com>
2020-12-07 10:53:25 -0300

    Remove some updates

---
+++ src/NavegacaoLateral.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+
+function NavegacaoLateral(){
+    return(
+        <Nav  className="justify-content-start">
+            <Nav.Item>
+                <h1>Preço</h1>
+                
+            </Nav.Item>
+        </Nav>
+    );
+}
+
+export default NavegacaoLateral;
