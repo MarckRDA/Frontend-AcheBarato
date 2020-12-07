0a4cb61c3f8a2824da3e3a7bede045343eb2fc53
MarckRDA <marcos.edo132@gmail.com>
2020-12-07 11:06:50 -0300

    update navegação lateral

--- src/NavegacaoLateral.jsx
+++ src/NavegacaoLateral.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Nav } from 'react-bootstrap';
+import React from "react";
+import { Nav } from "react-bootstrap";
 
-function NavegacaoLateral(){
-    return(
-        <Nav  className="justify-content-start">
-            <Nav.Item>
-                <h1>Preço</h1>
-                
-            </Nav.Item>
-        </Nav>
-    );
+function NavegacaoLateral() {
+  return (
+    <Nav className="justify-content-start">
+      <Nav.Item>
+        <h1>Preço</h1>
+      </Nav.Item>
+    </Nav>
+  );
 }
 
 export default NavegacaoLateral;
