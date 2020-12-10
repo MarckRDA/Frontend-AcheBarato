1941647b90d142f46178a7c7e550a759eddd89da
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-12-10 08:54:07 -0300

    creating Routers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import "./App.css";
 import Login from "./Components/Login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Image, Jumbotron, Row } from "react-bootstrap";
-<<<<<<< HEAD
-=======
 import "./Components/styleLogo.css" 
 
->>>>>>> e422ab45d533bdb1b95686266d244fd66d7f125f
 import MainPage from "./MainPage";
 import ProdutosPesquisados from "./ProdutosPesquisados";
 import NavegacaoLateral from "./NavegacaoLateral";
@@ -17,32 +14,18 @@ function App() {
   return (
     //<PrimarySearchAppBar/>
 
-<<<<<<< HEAD
-  
-      // <Container >
-      //   <Row>
-      //     <Col xs={6}>
-      //     </Col>
-      //     <Col xs={6}>
-=======
       // <Container >
       //   <Row>
       //     <Col xs={6}  >
       //     </Col>
       //     <Col xs={6} >
->>>>>>> e422ab45d533bdb1b95686266d244fd66d7f125f
       //       <Login />
       //     </Col>
       //   </Row>
       // </Container>
-<<<<<<< HEAD
-   
-    //<MainPage/>
-=======
      
 
     <MainPage/>
->>>>>>> e422ab45d533bdb1b95686266d244fd66d7f125f
     // <Container className="mr-auto ml-auto ">
     //   <Row>
     //     <Col xs={2}>
@@ -56,11 +39,7 @@ function App() {
     //   </Row>
     // </Container>
     // <Login/>
-<<<<<<< HEAD
-    <ProdutoEscolhido/>
-=======
     // <ProdutoEscolhido/>
->>>>>>> e422ab45d533bdb1b95686266d244fd66d7f125f
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import MainPage from './MainPage';
+import NavegacaoLateral from './NavegacaoLateral';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+  <Switch>
+      <Route path="/App" exact={true} component={App} />
+      <Route path="/MainPage" exact={true} component={MainPage} />
+      <Route path="/NavegacaoLateral" component={NavegacaoLateral} />
+      <Route path="/PrimarySearchAppBar" component={Primary} />
+      <Route path="/NavegacaoLateral" component={NavegacaoLateral} />
+      <Route path="/NavegacaoLateral" component={NavegacaoLateral} />
+      <Route path="/NavegacaoLateral" component={NavegacaoLateral} />
+
+  </Switch>
+</ BrowserRouter>
+
+, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
