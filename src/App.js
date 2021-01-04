9f83b8e34de15f2a94afd2db706f43a8c196b3d0
rodrigocamargo <rodrigocamargo854@gmail.com>
2021-01-04 10:25:13 -0300

    components organization

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ProdutoEscolhido from './UI/Pages/ProdutoEscolhido.jsx'
+import Login from './Components/Login/Login.jsx'
 
 // todo : to make a component call routers and make to render here
 function App() {
-  return (
+    return (
 
-      // <Container >
-      //   <Row>
-      //     <Col xs={6}  >
-      //     </Col>
-      //     <Col xs={6} >
-      //       <Login />
-      //     </Col>
-      //   </Row>
-      // </Container>
-    //  <Login/>
+        // <
+        // Container >
+        //   <Row>
+        //     <Col xs={6}  >
+        //     </Col>
+        //     <Col xs={6} >
+        <
+        Login / >
+        //     </Col>
+        //   </Row>
+        // </Container>
 
-     <ProdutoEscolhido products={[
-      { name: "cyberpunk", price: "24,20"},
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" },
-      { name: "shbah", price: "43,23" }]}/>
-  );
+
+
+        // <
+        // ProdutoEscolhido products = {
+        //     [
+        //         { name: "cyberpunk", price: "24,20" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" },
+        //         { name: "shbah", price: "43,23" }
+        //     ]
+        // }
+        // />
+    );
 }
 
 export default App;
