27ace440d1302180298833d9ce8e08fdaa4473d1
MarckRDA <marcos.edo132@gmail.com>
2021-01-15 18:42:18 -0300

    Files in localstore

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD
-import ProdutoEscolhido from './UI/Pages/ProdutoEscolhido.jsx'
-import MainPage from './UI/Pages/MainPage.jsx'
-=======
 import Login from './Components/Login/Login.jsx'
 import FormRegister from './Components/FormRegister/FormRegister.jsx'
 import BrandButton from './Components/BrandButton/BrandButton.jsx'
 import MainPage from './UI/Pages/MainPage.jsx';
 import Routes from './Routes.jsx';
 import BannerPropaganda from './Components/Banner/BannerPropaganda'
->>>>>>> 597135a79a076ebe76d9aa6bf3fddc557cd88db5
 // todo : to make a component call routers and make to render here
 function App() {
     return (
@@ -32,34 +27,6 @@ function App() {
         // <
         // BrandButton / >
 
-<<<<<<< HEAD
-      // <Container >
-      //   <Row>
-      //     <Col xs={6}  >
-      //     </Col>
-      //     <Col xs={6} >
-      //       <Login />
-      //     </Col>
-      //   </Row>
-      // </Container>
-    //  <Login/>
-    <MainPage/>
-  //    <ProdutoEscolhido products={[
-  //     { name: "cyberpunk", price: "24,20"},
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" },
-  //     { name: "shbah", price: "43,23" }]}/>
-   );
-=======
         // <
         // ProdutoEscolhido products = {
         //     [
@@ -85,7 +52,6 @@ function App() {
         // BannerPropaganda / >
 
     );
->>>>>>> 597135a79a076ebe76d9aa6bf3fddc557cd88db5
 }
 
 export default App;
