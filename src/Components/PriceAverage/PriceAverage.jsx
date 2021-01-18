5493d5257277619581b0f5a43caafb8901aeaebf
rodrigocamargo <rodrigocamargo854@gmail.com>
2021-01-17 21:26:21 -0300

    PriceAverage without search bar

--- src/Components/PriceAverage/PriceAverage.jsx
+++ src/Components/PriceAverage/PriceAverage.jsx
@@ -5,6 +5,7 @@ import CardResultProducts from "../CardResultProducts/CardResultProducts.jsx";
 import Menu from "../Menu";
 import { MenuAside } from '../PriceAverage/PriceAverage';
 import Footer from '../Footer/Footer.jsx';
+import CarouselProducts from "../CarouselProducts/CarouselProducts.jsx";
 
 
 const PriceAverage = () => {
@@ -14,18 +15,32 @@ const PriceAverage = () => {
         <div class="menu">
           <Menu />
 I       <div>
+  <div>
+  <CarouselProducts trendingProducts={[
+        { name: "cyberpunk", description: "xsxs", category: "eletronicos" },
+        { name: "shbah", description: "dsjbasj", category: "eletronicos" },
+        { name: "ssjnjn", description: "kmdskmd", category: "eletronicos" },
+        { name: "hbahasa", description: "dkdmskmd", category: "eletronicos" },
+        { name: "hbahasa", description: "dkdmskmd", category: "roupas" },
+        { name: "hbahasa", description: "dkdmskmd", category: "roupas" },
+        { name: "hbahasa", description: "dkdmskmd", category: "roupas" },
+        { name: "hbahasa", description: "dkdmskmd", category: "roupas" },
+        { name: "hbahasa", description: "dkdmskmd", category: "computadores" },
+        { name: "hbahasa", description: "dkdmskmd", category: "computadores" },
+        { name: "hbahasa", description: "dkdmskmd", category: "computadores" },
+        { name: "hbahasa", description: "dkdmskmd", category: "computadores" }
+      ]} />
+  </div>
             <aside class="animate-right">
               <MenuAside />
             </aside>
           </div>
           <Footer />
         </div>
-        <div class="technical-info">
-          <article>
-            <CardResultProducts/>
-          </article>
-        </div>
+      
+       
       </body>
+      
       
     </>
   );
