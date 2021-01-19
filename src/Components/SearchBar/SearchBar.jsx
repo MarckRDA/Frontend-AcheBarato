f542d52ef806fd48a0bc6775ddb8f6511ea484fa
MarckRDA <marcos.edo132@gmail.com>
2021-01-19 15:39:17 -0300

    search bar component is working

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,23 +1,33 @@
-import { InputGroup,Form} from "react-bootstrap";
-import {ButtonStyled,FormControlStyled} from "./SearchBar.js";
-import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { InputGroup, Form } from "react-bootstrap";
+import { ButtonStyled, FormControlStyled } from "./SearchBar.js";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import PriceAverage from '../PriceAverage/PriceAverage.jsx'
 
 const SearchBar = () => {
-  return (
-    <>
-      <InputGroup className="mr-auto ml-auto col-sm-9 ">
-        <FormControlStyled
-          placeholder="Search a product"
-          aria-label="Search a product"
-          aria-describedby="basic-addon2"
-        />
-        <Link to='/ProdutosPesquisados'>
-        <ButtonStyled variant="outline-primary">Search</ButtonStyled> 
-        </Link>
-      </InputGroup>
+  const [search, setSearch] = useState("");
 
-    </>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(search);
+  };
+
+  return (
+    
+      <form className="mr-auto ml-auto col-sm-9 " onSubmit={handleSubmit}>
+        <InputGroup>
+          <FormControlStyled
+            placeholder="Search for a product"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <Link to= {search === '' ? '/MainPage' : `/PriceAverage/${search}`}>
+            <ButtonStyled variant="outline-primary">Search</ButtonStyled>
+          </Link>
+        </InputGroup>
+      </form>
+     
+
   );
 };
 
