05e397c9dbf823f082ebebaab120a3fa74f09235
MarckRDA <marcos.edo132@gmail.com>
2021-01-19 08:15:31 -0300

    Added axios instance

--- src/Components/CarouselProducts/CarouselProducts.jsx
+++ src/Components/CarouselProducts/CarouselProducts.jsx
@@ -1,36 +1,53 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { CarouselItem, Carousel, Container, Row } from "react-bootstrap";
-import instanceAxios from "../../axios/axios";
+import axios from "axios";
 import CardTrendProduct from "../CardTrendProduct/CardTrendProduct";
 
-
-function CarouselProducts(props) {
+function CarouselProducts() {
   const [products, setProducts] = useState([]);
-  
+
   useEffect(() => {
     async function loadBooks() {
-      const response = await instanceAxios.get('/trendproducts');
-  
+      const response = await axios.get(
+        "https://localhost:5001/achebarato/products/trendproducts"
+      );
+
       setProducts(response.data);
     }
-  
+
     loadBooks();
   }, []);
 
-  
-
+  let index = 0;
+ 
   const CardsTrendsProdutcs = products.map((product) => (
     <CardTrendProduct
-      key={product.id_product.tostring()}
+      key={product.id_product}
       productName={product.name}
+      productPrice={product.price}
       productThumbImage={product.thumbImgLink}
     />
   ));
 
-  const cards = [1, 1, 1, 1, 1, 1, 1].map((n) => (
+  const cards = [1,1].map((n) => (
     <CarouselItem key={n.toString()}>
       <Container>
-        <Row className="ml-auto mr-auto mb-3">{CardsTrendsProdutcs}</Row>
+        <Row className="ml-auto mr-auto mb-3">
+          {products.map((product) => {
+           index = index  + 1;
+            if (product.cathegory.name === "Computadores")
+              return (
+                <CardTrendProduct
+                  key={product.id_product}
+                  productName={product.name}
+                  productPrice={product.price}
+                  productThumbImage={product.thumbImgLink}
+                />
+              )
+              ;
+              
+          })}
+        </Row>
       </Container>
     </CarouselItem>
   ));
