88c412ea4967043b787e0c09f81a8c1561b15916
MarckRDA <marcos.edo132@gmail.com>
2021-01-19 21:54:36 -0300

    Updated button onSubmit

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Form, Button, Jumbotron } from "react-bootstrap";
-import "./LoginStyles.css"
-import { LoginPage, LoginButton,JumbotronStyled} from "./loginstyles";
-import styled from "styled-components";
-import imagem from "../assets/logoicone.png"
-import backimage from "../assets/backimage.png"
+import { Form } from "react-bootstrap";
+import "./LoginStyles.css";
+import { LoginPage, LoginButton, JumbotronStyled } from "./loginstyles";
+import imagem from "../assets/logoicone.png";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -13,48 +11,57 @@ const Login = () => {
 
   return (
     <>
-    <LoginPage>
-        <JumbotronStyled >
-        <Link to='/MainPage'>
-      <img class="img-login" src={imagem} alt=""/>
+      <LoginPage>
+        <JumbotronStyled>
+          <Link to="/MainPage">
+            <img class="img-login" src={imagem} alt="" />
           </Link>
-        <Form >
-        <h3 class="h3-login">Login</h3>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label >Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="E-mail"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </Form.Group>
+          <Form>
+            <h3 class="h3-login">Login</h3>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword" >
-            <Form.Label >Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={passwd}
-              onChange={(event) => setPasswd(event.target.value)}
-            />
-          </Form.Group>
-          <Form.Group>
-            <LoginButton variant="outline-primary" type="submit" color="primary" block>
-              Login
-          </LoginButton>
-
-              <LoginButton variant="outline-primary"
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={passwd}
+                onChange={(event) => setPasswd(event.target.value)}
+              />
+            </Form.Group>
+            <Form.Group>
+              <LoginButton
+                variant="outline-primary"
                 type="submit"
                 color="primary"
-                block>
-                Register
-            </LoginButton>
-          </Form.Group>
-        </Form>
-      </JumbotronStyled>
-  </LoginPage>
-  </>
+                block
+              >
+                Login
+              </LoginButton>
+              <br/>
+              <Link to='/FormRegister'>
+                <LoginButton
+                  variant="outline-primary"
+                  type="submit"
+                  color="primary"
+                  block
+                >
+                  Register
+                </LoginButton>
+              </Link>
+            </Form.Group>
+          </Form>
+        </JumbotronStyled>
+      </LoginPage>
+    </>
   );
 };
 
