584349493069090b9f22d231af610fc9ab7307c4
MarckRDA <marcos.edo132@gmail.com>
2021-01-23 10:19:57 -0300

    Fixed css elements

--- src/Components/AlertPrice/AlertPrice.jsx
+++ src/Components/AlertPrice/AlertPrice.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 const AlarmButton = styled.button`
@@ -7,7 +7,6 @@ const AlarmButton = styled.button`
   border: none !important;
   width: 255px;
   border-radius: 8px;
-
   height: 43px;
 
   &:hover {
@@ -19,38 +18,57 @@ const AlarmButton = styled.button`
   }
 `;
 const FlexContainer = styled.div`
-  justify-content: flex-start;
-  max-width: 300px;
-  flex: 1 1 300px;
+  display: inline;
+  flex: initial;
   font-size: 30px;
   padding: 0 10px 0 20px;
   color: #999;
   border: 2px solid #ccc;
-  border-right: 0;
-  line-height: 2.5;
-  border-radius: 7px 0 0 7px;
+  line-height: 1.5;
+  border-radius: 7px;
   background: #fff;
 `;
 
 const InputPrice = styled.input`
-  width: 255px;
+  width: 155px;
   border-radius: 10px;
   border: none;
+  color: #999;
   height: 43px;
   padding: 20px;
+  display: inline-block;
   &:focus {
     outline: none;
   }
 `;
 
+const ContainerAligh = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+
+
 const AlertPrice = () => {
+  
+  const [price, setPrice] = useState(0);
+  
+  const handleAlertPrice = () =>(
+    console.log(`enviando esse preço: ${price}`)
+  )
+  
   return (
     <>
-      <FlexContainer    >
-        <span>R$</span>
-        <InputPrice type="number" min="1.0" step="0.10" max="99999" />
-      </FlexContainer>
-      <AlarmButton size="lg">Avise-me !</AlarmButton>
+      <h2>Qual preço deseja pagar no produto?</h2>
+      <ContainerAligh>
+        <FlexContainer style={{ marginRight: 10 }}>
+          <span>R$</span>
+          <InputPrice type="number" min="1.0" step="0.10" max="99999" value={price} onChange={(e) => setPrice(e.target.value)} />
+        </FlexContainer>
+        <AlarmButton size="lg" onClick={handleAlertPrice}>Avise-me !</AlarmButton>
+      </ContainerAligh>
     </>
   );
 };
