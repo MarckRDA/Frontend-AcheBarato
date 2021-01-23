01405ffaadb364d01940a237abef5facfec65faa
MarckRDA <marcos.edo132@gmail.com>
2021-01-22 22:32:03 -0300

    Added AlertPrice.jsx

--- src/Components/PriceAverage/Charts/PriceCharts.jsx
+++ src/Components/PriceAverage/Charts/PriceCharts.jsx
@@ -2,46 +2,41 @@ import * as React from "react";
 import "./PriceCharts.css";
 import { Chart } from "react-google-charts";
 
+const PriceCharts = (props) => {
+  const dataArray = [["Variação de preço", "Variação de Preços"]];
 
-const PriceCharts = () => {
+  const output = props.data.map(({ dateOfPrice, priceOfThatDay }) =>
+  dataArray.push([dateOfPrice, priceOfThatDay])
+  );
+
   return (
-
     <Chart
-
       chartType="ColumnChart"
       spreadSheetUrl="https://docs.google.com/spreadsheets/d/1jN0iw0usssnsG1_oi-NXtuKfsUsGme09GsFidbqxFYA"
       toolbarItems={[
         {
-          type: 'csv',
+          type: "csv",
           datasource:
-            'https://spreadsheets.google.com/tq?key=1jN0iw0usssnsG1_oi-NXtuKfsUsGme09GsFidbqxFYA',
+            "https://spreadsheets.google.com/tq?key=1jN0iw0usssnsG1_oi-NXtuKfsUsGme09GsFidbqxFYA",
         },
       ]}
-      rootProps={{ 'data-testid': '1' }}
-
+      rootProps={{ "data-testid": "1" }}
       width={400}
-      height={'300px'}
+      height={"300px"}
       chartType="AreaChart"
       loader={<div>Loading Chart</div>}
-      data={[
-        ['Variação de preço', 'Variação de Preços'],
-        ['data1', 1000],
-        ['data2', 1170],
-        ['data3', 660],
-        ['data4', 1030],
-      ]}
+      data={dataArray}
       options={{
-        title: 'Preço por data',
-        hAxis: { title: 'Datas', titleTextStyle: { color: '#0b34eb' } },
+        title: "Historico de Preços",
+        hAxis: { title: "Datas", titleTextStyle: { color: "#0b34eb" } },
         vAxis: { minValue: 0 },
 
         // For the legend to fit, we make the chart area smaller
-        chartArea: { width: '50%', height: '70%' },
+        chartArea: { width: "50%", height: "70%" },
         // lineWidth: 25
       }}
     />
-
-
   );
 };
+
 export default PriceCharts;
--- src/UI/Pages/ProdutoEscolhido.jsx
+++ src/UI/Pages/ProdutoEscolhido.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import Carrousel from "../../Components/CarouselMainPage/CarouselMainPage";
+import { Accordion, Badge } from "react-bootstrap";
+
 import { useParams } from "react-router-dom";
 import CardListProduct from "../../Components/CardListProduct/CardListProduct";
-import PriceAverage from "../../Components/PriceAverage/PriceAverage";
 import TechnicalInformation from "../../Components/TecnicalInformation/TechnicalInformation";
-
 import NavSuperior from "../../Components/MenuSearchBar/index";
-
 import axios from "axios";
+import PriceCharts from "../../Components/PriceAverage/Charts/PriceCharts";
 import CarouselPictureSelected from "../../Components/CarouselPictureSelected/CarouselPictureSelected";
+import AlertPrice from "../../Components/AlertPrice/AlertPrice";
 
 const ProdutoEscolhido = () => {
   const [productSelected, setProductSelected] = useState([]);
@@ -18,7 +18,6 @@ const ProdutoEscolhido = () => {
   let { id } = useParams();
 
   useEffect(() => {
-    
     async function loadRelatedProducts() {
       await axios
         .all([
@@ -40,45 +39,76 @@ const ProdutoEscolhido = () => {
     loadRelatedProducts();
   }, []);
 
+  const productRelatedPartOne = relatedProducts.slice(
+    Math.max(relatedProducts.length - 5, 1)
+  );
 
   return (
     <>
       <NavSuperior />
       <Container className="ml-auto mr-auto mt-3">
-        <Row>
-          {isLoaded && (
-            <>
-              <Col xs={6}>
-                <CarouselPictureSelected pictures={productSelected.pictures} />
-              </Col>
-              <Col xs={6} className="align-self-center justify-content-start">
-                <h1 className="h1">{productSelected.name}</h1>
-                <h3>R$ {productSelected.price}</h3>
-                <a href={productSelected.linkRedirectShop}>
-                  <Button variant="primary">Link para loja</Button>
-                </a>
-              </Col>
-            </>
-          )}
-        </Row>
-        <h1>Produtos relacionados</h1>
-        {isLoaded &&
-          relatedProducts.map((relatedProduct) => (
-            <CardListProduct
-              key={relatedProduct.id_product}
-              productImg={relatedProduct.thumbImgLink}
-              productName={relatedProduct.name}
-              productPrice={relatedProduct.price}
+        {isLoaded && (
+          <>
+            <Row>
+              {
+                <>
+                  <Col xs={6}>
+                    <CarouselPictureSelected
+                      pictures={productSelected.pictures}
+                    />
+                  </Col>
+                  <Col
+                    xs={6}
+                    className="align-self-center justify-content-start"
+                  >
+                    <h1 className="h1">{productSelected.name}</h1>
+                    <h3>R$ {productSelected.price}</h3>
+                    <a href={productSelected.linkRedirectShop}>
+                      <Button variant="primary">Link para loja</Button>
+                    </a>
+                  </Col>
+                </>
+              }
+            </Row>
+            <PriceCharts data={productSelected.historycalṔrices} />
+            <br />
+
+            <AlertPrice />
+            <br />
+            <br />
+            <TechnicalInformation
+              productInformations={productSelected.descriptions}
             />
-          ))}
-        <br />
-        <br />
-        {/* <PriceAverage /> */}
-        <br />
-        <br />
-        {isLoaded && <TechnicalInformation
-          productInformations={productSelected.descriptions}
-        />}
+            <br />
+            <br />
+            <h1>Produtos relacionados</h1>
+            {productRelatedPartOne.map((relatedProduct) => (
+              <CardListProduct
+                key={relatedProduct.id_product}
+                productImg={relatedProduct.thumbImgLink}
+                productName={relatedProduct.name}
+                productPrice={relatedProduct.price}
+                productLinkRedirec={relatedProduct.linkRedirectShop}
+              />
+            ))}
+            <Accordion defaultActiveKey="1">
+              {relatedProducts.map((relatedProduct) => (
+                <Accordion.Collapse eventKey="0">
+                  <CardListProduct
+                    key={relatedProduct.id_product}
+                    productImg={relatedProduct.thumbImgLink}
+                    productName={relatedProduct.name}
+                    productPrice={relatedProduct.price}
+                    productLinkRedirec={relatedProduct.linkRedirectShop}
+                  />
+                </Accordion.Collapse>
+              ))}
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                <Badge variant="primary">Ver mais</Badge>{" "}
+              </Accordion.Toggle>
+            </Accordion>
+          </>
+        )}
       </Container>
     </>
   );
