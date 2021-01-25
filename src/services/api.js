96636e51e52a31c2d7fd7c0177649307b229f65c
MarckRDA <marcos.edo132@gmail.com>
2021-01-24 21:02:31 -0300

    axios.get methot Abstracted

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,32 @@
 import axios from "axios";
-import { getToken } from "./auth";
+
+const TOKEN_USER = "token";
+const USER_ID = "UserId";
 
 const api = axios.create({
   baseURL: "https://localhost:5001/achebarato",
   headers: { "content-type": "application/json" },
 });
 
-const 
+api.interceptors.request.use((config) => {
+  config.headers.Autorization = `Bearer ${localStorage.getItem(TOKEN_USER)}`;
 
-api.interceptors.request.use(config =>{
-  config.headers
-})
+  return config;
+});
+
+export const login = ({ email, password }) => {
+  return api.post("/users/login", { email, password }).then((response) => {
+    localStorage.setItem(TOKEN_USER, response.data.token);
+    localStorage.setItem(USER_ID, response.data.userId);
+  });
+};
+
+export const signUp = ({ name, email, password }) => {
+  return api
+    .post("/users", { name, email, password })
+    .then((response) => localStorage.setItem(USER_ID, response.data));
+};
+
+export const get = (url) => {
+  return api.get(url);
+}
