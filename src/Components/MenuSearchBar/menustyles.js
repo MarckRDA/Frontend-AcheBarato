10812d274ca898a84b7ab534c3e4f2eb9184fa34
MarckRDA <marcos.edo132@gmail.com>
2021-01-26 18:41:14 -0300

    Created logout button

--- src/Components/MenuSearchBar/menustyles.js
+++ src/Components/MenuSearchBar/menustyles.js
@@ -2,27 +2,25 @@ import styled from "styled-components";
 import { Button } from "react-bootstrap";
 import imagem from "../assets/logoicone.png";
 
-
 export const LoginButton = styled(Button)`
-    
-    border: none;
-    margin-left:10rem;
-    `;
+  border: none;
+  margin-left: 10rem;
+`;
+
+export const LogOutButton = styled(Button)`
+  border: none;
+  margin-left: 10rem;
+`;
 
 export const SearchBarStyled = styled.div`
-    
-    width: 48rem;
-    margin-left: -11rem;
-    `;
+  width: 48rem;
+  margin-left: -11rem;
+`;
 export const SigInButton = styled(Button)`
-
-border: none;
-margin-left:10px;
+  border: none;
+  margin-left: 10px;
 `;
 
 export const Imagem = styled(imagem)`
-
-max-width: 11rem;
-
+  max-width: 11rem;
 `;
-
