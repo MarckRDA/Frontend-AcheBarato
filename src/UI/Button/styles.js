d5b77e9d0b016aa261293559dfc1a56c8170544b
rodrigocamargo <rodrigocamargo854@gmail.com>
2021-01-26 21:22:42 -0300

    color changed

--- src/UI/Button/styles.js
+++ src/UI/Button/styles.js
@@ -8,3 +8,20 @@ display: flex;
 margin-left: 1rem;
 
 `;
+
+const GlobalButton = styled.button`
+  color: #ff6633;
+  background-color: rgba(255, 102, 51, 0);
+  border: none !important;
+  width: 255px;
+  border-radius: 8px;
+  height: 43px;
+
+  &:hover {
+    background-color: #ff6633;
+    color: #fff;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
