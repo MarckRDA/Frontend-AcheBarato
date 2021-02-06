3c23434124ed115e6e070a79633fc3dad1ffaec1
MarckRDA <marcos.edo132@gmail.com>
2021-02-06 16:39:32 -0300

    Cleaned up  useless spaces

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -6,27 +6,25 @@ import { Link } from "react-router-dom";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
-  
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
   return (
-    
-      <form className="mr-auto ml-auto col-sm-9 " onSubmit={handleSubmit}>
-        <InputGroup>
-          <FormControlStyled
-            placeholder="Search for a product"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <Link to= {search === '' ? '/' : `/PageResults?search=${search}`}>
-            <ButtonStyled variant="outline-primary">Search</ButtonStyled>
-          </Link>
-        </InputGroup>
-      </form>
-     
-
+    <form className="mr-auto ml-auto col-sm-9 " onSubmit={handleSubmit}>
+      <InputGroup>
+        <FormControlStyled
+          placeholder="Search for a product"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Link to={search === "" ? "/" : `/PageResults?search=${search}`}>
+          <ButtonStyled variant="outline-primary">Search</ButtonStyled>
+        </Link>
+      </InputGroup>
+    </form>
   );
 };
 
