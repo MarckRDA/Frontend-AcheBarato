77f2fd25289991501183d587e8d168c25dd6ffd6
MarckRDA <marcos.edo132@gmail.com>
2021-02-06 16:38:51 -0300

    Put getUserPreferences

--- src/Components/PriceAverage/PriceAverage.jsx
+++ src/Components/PriceAverage/PriceAverage.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import {
+  getSearchedProducts, 
+  postUserPreferences
+} from "../../services/api.js";
 import { useLocation } from "react-router";
+import useAuth from '../../Context/hooks/useAuth';
 import { Col, Container, Row } from "react-bootstrap";
 import MenuSearchBar from "../MenuSearchBar";
 import MenuAside from "../MenuAside/index";
@@ -15,32 +19,51 @@ const PriceAverage = () => {
   const [maxPrice, setMaxPrice] = useState(99999);
   const [ordering, setOrdering] = useState(" ");
 
+  const { isAuthenticated } = useAuth();
+
+
   function useQuery() {
     return new URLSearchParams(useLocation().search);
   }
 
   let urlParams = useQuery();
 
+  const search = urlParams.get("search");
+
   useEffect(() => {
     async function loadProducts() {
-      const response = await axios.get(
-        `https://localhost:5001/achebarato/products?Search=${urlParams.get(
-          "search"
-        )}&MinPrice=${minPrice}&MaxPrice=${maxPrice}&OrderBy=${ordering}`
-      );
-
+      const response = await getSearchedProducts({
+        search,
+        minPrice,
+        maxPrice,
+        ordering,
+      });
       setProducts(response.data);
       setIsLoaded(true);
     }
+   
 
     loadProducts();
   }, [minPrice, maxPrice, ordering]);
+
+  useEffect(() => {
+    function getUserPreference() {
+      postUserPreferences( search );
+    }
+    
+    if (isAuthenticated) {
+      getUserPreference(search);
+    }
+
+  });
 
   const handlerFilters = (minprice, maxprice, orderBy) => {
     setMinPrice(minprice);
     setOrdering(orderBy);
     setMaxPrice(maxprice);
   };
+
+  console.log(isAuthenticated);
 
   return (
     <div class="menu ">
@@ -53,13 +76,21 @@ const PriceAverage = () => {
           {isLoaded ? (
             <ProdutosPesquisados products={products} />
           ) : (
-            <Col xl={10} xs={10} style={{marginLeft: 'auto', marginRight: 'auto', display: 'block'}}>
-            <ReactLoading
-              type="balls"
-              color="#ff6633"
-              height={200}
-              width={200}
-            />
+            <Col
+              xl={10}
+              xs={10}
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "block",
+              }}
+            >
+              <ReactLoading
+                type="balls"
+                color="#ff6633"
+                height={200}
+                width={200}
+              />
             </Col>
           )}
         </Row>
