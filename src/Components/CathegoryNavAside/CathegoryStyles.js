94e4b85ea91dac2158e51b070e3ee09d1e740571
MarckRDA <marcos.edo132@gmail.com>
2021-02-06 10:53:49 -0300

    Deleted useless files

--- src/Components/CathegoryNavAside/CathegoryStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { Nav } from "react-bootstrap";
-
-
-export const NavStyles = styled(Nav)`
-    font-weight: 500;
-    font-family: 'Fira Sans', sans-serif;
-`
--- src/Components/SliderBarPrices/sliderstyles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-
-export const SliderStyled = styled.div`
-margin: auto
-`
-
