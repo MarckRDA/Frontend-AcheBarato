a7803d4d070964a718bceecffbbc8ee848ad6206
MarckRDA <marcos.edo132@gmail.com>
2021-02-14 14:57:42 -0300

    Added pagination component

--- src/Components/PriceAverage/PriceAverage.jsx
+++ src/Components/PriceAverage/PriceAverage.jsx
@@ -1,26 +1,25 @@
 import { useState, useEffect } from "react";
-import {
-  getSearchedProducts, 
-  postUserPreferences
-} from "../../services/api.js";
+import { getSearchedProducts } from "../../services/api.js";
 import { useLocation } from "react-router";
-import useAuth from '../../Context/hooks/useAuth';
 import { Col, Container, Row } from "react-bootstrap";
 import MenuSearchBar from "../MenuSearchBar";
 import MenuAside from "../MenuAside/index";
 import Footer from "../Footer/Footer.jsx";
 import ProdutosPesquisados from "../ProdutosPesquisados/ProdutosPesquisados";
 import ReactLoading from "react-loading";
+import Pagination from "react-js-pagination";
 
 const PriceAverage = () => {
   const [products, setProducts] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(99999);
+  const [itemsPerPage, setItemsPerPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+  const [pageNumber, setpageNumber] = useState(1);
+  const [activePage, setActivePage] = useState(1);
   const [ordering, setOrdering] = useState(" ");
-
-//  const { isAuthenticated } = useAuth();
-
 
   function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -36,26 +35,20 @@ const PriceAverage = () => {
       minPrice,
       maxPrice,
       ordering,
+      pageNumber,
     });
     setProducts(response.data.data);
+    setItemsPerPage(response.data.limit);
+    setTotalPage(response.data.totalPages);
+    setTotalItems(response.data.total);
+
     setIsLoaded(true);
   }
- 
+
   useEffect(() => {
-
     loadProducts();
-  }, [minPrice, maxPrice, ordering]);
-
-  // useEffect(() => {
-  //   function getUserPreference() {
-  //     postUserPreferences( search );
-  //   }
-    
-  //   if (isAuthenticated) {
-  //     getUserPreference(search);
-  //   }
-
-  // });
+    return setIsLoaded(false);
+  }, [minPrice, maxPrice, ordering, pageNumber]);
 
   const handlerFilters = (minprice, maxprice, orderBy) => {
     setMinPrice(minprice);
@@ -63,6 +56,10 @@ const PriceAverage = () => {
     setMaxPrice(maxprice);
   };
 
+  const handlerPagination = (page) => {
+    setpageNumber(page);
+    setActivePage(page);
+  };
 
   return (
     <div class="menu ">
@@ -73,7 +70,9 @@ const PriceAverage = () => {
             <MenuAside onChangeFilters={handlerFilters} />
           </aside>
           {isLoaded ? (
-            <ProdutosPesquisados products={products} />
+            <>
+              <ProdutosPesquisados products={products} />
+            </>
           ) : (
             <Col
               xl={10}
@@ -92,6 +91,17 @@ const PriceAverage = () => {
               />
             </Col>
           )}
+          <Col xl={12} xs={12} style={{ padding: "10px 50%" }}>
+            <Pagination
+              activePage={activePage}
+              itemsCountPerPage={itemsPerPage}
+              totalItemsCount={totalItems}
+              pageRangeDisplayed={totalPage}
+              onChange={handlerPagination}
+              itemClass="page-item"
+              linkClass="page-link"
+            />
+          </Col>
         </Row>
         <Footer />
       </Container>
