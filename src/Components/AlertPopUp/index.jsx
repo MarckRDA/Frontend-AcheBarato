54aa212834b88cab08c659aa94c23a00f4c4ea19
MarckRDA <marcos.edo132@gmail.com>
2021-02-14 14:53:58 -0300

    Added onClose method

--- src/Components/AlertPopUp/index.jsx
+++ src/Components/AlertPopUp/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Toast } from "react-bootstrap";
 import styled from 'styled-components';
 
@@ -7,8 +7,11 @@ const AlertPopupBox = styled(Toast)`
 `
 
 const AlertPopup = ({ text }) => {
+  const [showA, setShowA] = useState(true);
+  const toggleShowA = () => setShowA(!showA);
+
   return (
-    <AlertPopupBox>
+    <AlertPopupBox show={showA} onClose={toggleShowA}>
       <Toast.Header>
         <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
         <strong className="mr-auto">AcheBarato</strong>
